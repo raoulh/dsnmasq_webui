78346df413ef331594625f09c713d82db57071a4
root <root@home-server.raoul.calaos.fr>
2015-08-15 10:37:54 +0200

    Fix connection of client

--- public/js/app.js
+++ public/js/app.js
@@ -4,15 +4,20 @@
 //
 **************************************/
 
-// connect to our socket server
-var socket = io.connect('http://127.0.0.1:6006/');
-
-var app = app || {};
-
 $(function(){
 
+	// connect to our socket server
+
+	var h = window.location.protocol + '//' + window.location.host;
+	console.log("connecting to " + h);
+	var socket = io.connect(h);
+
+	socket.on("connect", function() {
+		console.log("connected to server");
+	});
+
 	socket.on("leases", function(data) {
-		
+	console.log("new socket data:" + data);	
 		data.sort(function(a, b) {
 			
 			var aa = a.ip_address.split(".");
@@ -21,6 +26,7 @@ $(function(){
         	return ( aa[0]*0x1000000 + aa[1]*0x10000 + aa[2]*0x100 + aa[3]*1 )
              	 - ( bb[0]*0x1000000 + bb[1]*0x10000 + bb[2]*0x100 + bb[3]*1 );
 		});
+console.log("data sorted: " + data.length);
 		
 		$('#tableMachines tr').not(':first').remove();
 		var html = '';
@@ -33,6 +39,7 @@ $(function(){
 			
 			html += '<td>' + t.toLocaleString() + '</td></tr>';
 		}
+		console.log("add html: " + html);
 		$('#tableMachines tr').first().after(html);
 	});
 	
